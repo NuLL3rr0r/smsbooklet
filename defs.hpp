f35ec5d9aa5dfb66879a44b2570e6e6968bc06df
M.S. Babaei <info@babaei.net>
2013-10-24 05:23:30 +0330

    Missing defs.hpp file

---
+++ defs.hpp
@@ -0,0 +1,10 @@
+#ifndef DEFS_HPP
+#define DEFS_HPP
+
+
+#define  APP_TITLE           "کتابچه پیام"
+#define  ABOUT_BUTTON_TEXT   "درباره"
+#define  FAV_BUTTON_TEXT     "منتخب"
+
+
+#endif // DEFS_HPP
